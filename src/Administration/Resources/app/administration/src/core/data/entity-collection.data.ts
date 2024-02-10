1e148ee0334d951c78df57e15b46e01fdf4176fd
Tim Vallandi <t.vallandi@onacy.de>
2024-02-10 17:36:32 +0100

    Shopware 6.6 Init

---
+++ src/Administration/Resources/app/administration/src/core/data/entity-collection.data.ts
@@ -0,0 +1,8 @@
+/**
+ * @package admin
+ */
+
+import EntityCollection from '@shopware-ag/meteor-admin-sdk/es/data/_internals/EntityCollection';
+
+// eslint-disable-next-line sw-deprecation-rules/private-feature-declarations
+export default EntityCollection;
